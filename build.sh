b4110730553b1dbad5ac5b789551819bff2b2430
joao_arnold.py <joaopaulo167@gmail.com>
2023-12-22 16:33:26 -0300

    fix build/run script

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
-#Build on a macbook pro m1 
-!/bin/zsh
+#Build on a macbook pro m1
+#!/bin/zsh
 echo "Start Script!"
 echo "Building!"
 
@@ -8,9 +8,11 @@ IMGUI_DIR=include/imgui
 SFML_INCLUDE_DIR=include/sfml
 SFML_LIB_DIR=lib
 GLFW_DIR=include/GLFW
-output=$(g++ $PROG_NAME $IMGUI_DIR/imgui*.cpp \
-  -std=c++20 -Wno-unused-result \
-  -I $SFML_INCLUDE_DIR -I $IMGUI_DIR -I $GLFW_DIR \
+ROOT_DIR=src
+CPPFLAGS="-v -Wall -Wunused-result"
+output=$(g++ main.cpp $IMGUI_DIR/imgui*.cpp $ROOT_DIR/*.cpp \
+  -std=c++20 \
+  -I $SFML_INCLUDE_DIR -I $IMGUI_DIR -I $GLFW_DIR -I $ROOT_DIR \
   -L $SFML_LIB_DIR -lsfml-graphics -lsfml-window -lsfml-system  \
   -framework OpenGL \
   -o build.out \
@@ -23,6 +25,6 @@ else
   ./build.out
 fi
 echo "End Script!"
-echo "executable is located ./build.out"
+echo "Executable is located ./build.out"
 
 ### Command to execute -> ./build.sh main.cpp <-
